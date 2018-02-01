8c4958cfd676dcd3baf3ba802769210f29b0ec05
Anna Fock <anna@fock.se>
2018-02-01 10:52:16 +0100

    Sets HomeFrame as primarystage and opens file chooser on importbutton

--- Document Keeper/src/document/keeper/DocumentKeeper.java
+++ Document Keeper/src/document/keeper/DocumentKeeper.java
@@ -5,9 +5,12 @@
  */
 package document.keeper;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -22,30 +25,17 @@ public class DocumentKeeper extends Application
 {
     
     @Override
-    public void start(Stage primaryStage)
+    public void start(Stage primaryStage) throws IOException
     {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>()
-        {
-            
-            @Override
-            public void handle(ActionEvent event)
-            {
-                FileChooser fileChooser = new FileChooser();
-                fileChooser.setTitle("Open Resource File");
-                fileChooser.showOpenDialog(primaryStage);
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        Parent root = FXMLLoader.load(getClass().getResource("HomeFrame.fxml"));
         
-        Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        Scene scene = new Scene(root);
+        
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Document Keeper");
         primaryStage.show();
     }
 
--- Document Keeper/src/document/keeper/HomeFrameController.java
+++ Document Keeper/src/document/keeper/HomeFrameController.java
@@ -7,7 +7,13 @@ package document.keeper;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +22,16 @@ import javafx.fxml.Initializable;
  */
 public class HomeFrameController implements Initializable
 {
+    @FXML private Button importButton;
+    
+    @FXML
+    void handleImportButton(ActionEvent event){
+                Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open Resource File");
+                fileChooser.showOpenDialog(stage);
+    }
 
     /**
      * Initializes the controller class.
