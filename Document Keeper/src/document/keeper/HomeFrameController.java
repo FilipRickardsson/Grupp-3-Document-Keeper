b5557bcfd67c2026ada656d64f75c5f74f6f8feb
Alexiz Knöös <qwastanja@hotmail.com>
2018-02-01 14:11:09 +0100

    AK & H Views hardcoded data in listview

--- Document Keeper/src/document/keeper/HomeFrameController.java
+++ Document Keeper/src/document/keeper/HomeFrameController.java
@@ -14,15 +14,23 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -33,8 +41,14 @@ import javafx.stage.Stage;
  */
 public class HomeFrameController implements Initializable {
 
+    ArrayList fileList;
+    public static ObservableList <Document> obsDocumentList;
+    
     @FXML
     private Button importButton;
+    
+    @FXML
+    private ListView lvDocument;
 
     @FXML
     void handleImportButton(ActionEvent event) {
@@ -76,7 +90,23 @@ public class HomeFrameController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = "2018-02-01";
+        Date dateObject = new Date();
+        try {
+            dateObject = sdf.parse(dateString);
+        } catch (ParseException ex) {
+            Logger.getLogger(HomeFrameController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        fileList = new ArrayList();
+        fileList.add(new Document(1, "Cooper", ".txt", "54kb", dateObject, dateObject));
+        fileList.add(new Document(2, "Rose", ".doc", "100kb", dateObject, dateObject));
+        fileList.add(new Document(3, "Magnus", ".jpg", "12kb", dateObject, dateObject));
+        
+        obsDocumentList = FXCollections.observableArrayList(fileList);
+        lvDocument.setItems(obsDocumentList);
+        
     }
 
 }
