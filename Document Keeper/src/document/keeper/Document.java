dae5e46eb7702b0a1c3bc3785c6de3ad8a97e308
magnus <magnusnorlin@gmail.com>
2018-02-05 10:47:05 +0100

    AF o MN Doc class, dates in String

--- Document Keeper/src/document/keeper/Document.java
+++ Document Keeper/src/document/keeper/Document.java
@@ -12,13 +12,14 @@ public class Document {
     String title;
     String type;
     String file_size;
-    Date date_imported;
-    Date date_created;
+    String date_imported;
+    String date_created;
 
     public Document() {
     }
 
-    public Document(int id, String title, String type, String file_size, Date date_imported, Date date_created) {
+    public Document(int id, String title, String type, String file_size, String date_imported, String date_created)
+    {
         this.id = id;
         this.title = title;
         this.type = type;
@@ -27,7 +28,8 @@ public class Document {
         this.date_created = date_created;
     }
     
-    public Document(String title, String type, String file_size, Date date_imported, Date date_created) {
+        public Document(String title, String type, String file_size, String date_imported, String date_created)
+    {
         this.title = title;
         this.type = type;
         this.file_size = file_size;
@@ -35,57 +37,71 @@ public class Document {
         this.date_created = date_created;
     }
 
-    @Override
+        
+        @Override
     public String toString() {
         return title + "." + type;
-    }
-
-    public int getId() {
+    }    
+        
+    public int getId()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id)
+    {
         this.id = id;
     }
 
-    public String getTitle() {
+    public String getTitle()
+    {
         return title;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(String title)
+    {
         this.title = title;
     }
 
-    public String getType() {
+    public String getType()
+    {
         return type;
     }
 
-    public void setType(String type) {
+    public void setType(String type)
+    {
         this.type = type;
     }
 
-    public String getFile_size() {
+    public String getFile_size()
+    {
         return file_size;
     }
 
-    public void setFile_size(String file_size) {
+    public void setFile_size(String file_size)
+    {
         this.file_size = file_size;
     }
 
-    public Date getDate_imported() {
+    public String getDate_imported()
+    {
         return date_imported;
     }
 
-    public void setDate_imported(Date date_imported) {
+    public void setDate_imported(String date_imported)
+    {
         this.date_imported = date_imported;
     }
 
-    public Date getDate_created() {
+    public String getDate_created()
+    {
         return date_created;
     }
 
-    public void setDate_created(Date date_created) {
+    public void setDate_created(String date_created)
+    {
         this.date_created = date_created;
     }
+
 
 }
